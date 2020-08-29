a2d9b9bd7db6e9f4e0722f7ed02e20a50e428f34
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-08-29 20:51:34 -0300

    :sparkles: add image, inputs, text and button

---
+++ web/src/pages/Login/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import backgroundImg from '../../assets/images/Background.svg';
+import proffyLogo from '../../assets/images/Proffy.svg';
+import text from '../../assets/images/text.svg';
+
+import './styles.css';
+
+const Login: React.FC = () => {
+  return (
+    <div id="login-container">
+      <div className="background">
+        <img className="backgroundImg" src={backgroundImg} alt="Login" />
+        <div className="container-logo">
+          <img src={proffyLogo} alt="logo" />
+          <img src={text} alt="Texto" />
+        </div>
+      </div>
+
+      <div className="login">
+        <h1>Fazer login</h1>
+
+        <form>
+          <input
+            id="email"
+            className="text-login"
+            type="text"
+            placeholder="E-mail"
+          />
+          <input
+            id="password"
+            className="text-login"
+            type="text"
+            placeholder="Senha"
+          />
+
+          <div className="forget">
+            <label htmlFor="remember">
+              <input type="checkbox" id="remember" name="remember" />
+              Lembrar-me
+            </label>
+            <span>Esqueci minha senha</span>
+          </div>
+
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
